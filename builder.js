9cabacb86fd74892315a3827d017da63927c6a41
jeorogen <163756790+jeorogen@users.noreply.github.com>
2024-10-16 16:38:18 -0400

    Update builder.js

--- builder.js
+++ builder.js
@@ -174,7 +174,7 @@ window.addEventListener("load", (()=>{
                             }
                             const n = d.resize.left ? d.resize.x - t + l : t - d.resize.x
                               , a = d.resize.top ? d.resize.y - s + l : s - d.resize.y
-                              , o = Math.max(64, Math.min(n, a))
+                              , o = Math.max(64, Math.min(1))
                               , c = o - l;
                             d.resize.top && (d.element.style.top = parseFloat(d.element.style.top) - c + "px",
                             d.resize.y -= c),
